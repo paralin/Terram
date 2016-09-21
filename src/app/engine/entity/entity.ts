723324ad990e041b67173e4e257a8f9c23b01bcd
Christian Stewart <christian@paral.in>
2016-09-21 01:25:58 -0400

    feat(engine): instantiate go game properly
    This commit introduces instantiating gogame.Game properly from the global constructor. This is the
    
    first step towards implementing the game basics.

--- src/app/engine/entity/entity.ts
+++
@@ -1,6 +0,0 @@
-/*
- * An entity is a physics object paired with a Sprite.
- * The entity provides game logic decoupled from rendering.
- * It contains logic for networking and replication.
- */
-export class Entity {}
--- src/app/game/terram.ts
+++ src/app/game/terram.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Game } from '../engine/game';
+import * as TerramJsGlobal from '@fusebot/goterram';
 
 @Injectable()
 export class TerramGame extends Game {
+  private terramGame: any;
+
+  public init() {
+    // Expose for debugging
+    window['terramGameInstance'] = this;
+
+    super.init();
+
+    // Build an instance of TerramGame
+    this.terramGame = TerramJsGlobal.BuildTerramGame();
+  }
+
+  public destroy() {
+    this.terramGame.Destroy();
+    super.destroy();
+  }
 }
